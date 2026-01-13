9df389efce1782fb88c25ebf1f052ab062d5b40b
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-13 11:32:58 -0300

    Configured AppDbContext

--- src/WebApps/SocialNetwork.Write.API/Configs/DB/AppDbContext.cs
+++ src/WebApps/SocialNetwork.Write.API/Configs/DB/AppDbContext.cs
@@ -1,6 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Write.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using SocialNetwork.Write.API.Models.Bases;
+
 namespace SocialNetwork.Write.API.Configs.DB;
 
-public class AppDbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options)
+    : IdentityDbContext<UserModel, RoleModel, string>(options)
 {
+    public new DbSet<UserModel> Users { get; set; }
+    public new DbSet<RoleModel> Roles { get; set; }
     
+    public override int SaveChanges()
+    {
+        SetAuditDates();
+        return base.SaveChanges();
+    }
+    
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        SetAuditDates();
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    private void SetAuditDates()
+    {
+        var entries = ChangeTracker.Entries<BaseModel>();
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.Id = entry.Entity.Id ?? Guid.NewGuid().ToString();
+            }
+            else if (entry.State == EntityState.Modified)
+            {
+                entry.Entity.UpdatedAt = DateTime.UtcNow;
+            }
+        }
+    }
+ 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+
 }
