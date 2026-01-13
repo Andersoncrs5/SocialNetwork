beb4afd59657cf034755951661fb913d82ff2db4
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-13 20:34:26 -0300

    Configured UserMapper

---
+++ src/WebApps/SocialNetwork.Write.API/Utils/Mappers/UserMapper.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using SocialNetwork.Contracts.DTOs.User;
+using SocialNetwork.Write.API.Models;
+
+namespace SocialNetwork.Write.API.Utils.Mappers;
+
+public class UserMapper: Profile
+{
+    public UserMapper()
+    {
+        CreateMap<UserModel, UserDto>();
+    }
+}
