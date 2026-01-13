17a315a1502ade896fb071477844a9094d26732d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-13 20:17:14 -0300

    New annotation to validate Email

---
+++ src/BuildingBlocks/SocialNetwork.Contracts/Attributes/Globals/EmailConstraintAttribute.cs
@@ -0,0 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork.Contracts.Attributes.Globals;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
+public class EmailConstraintAttribute : ValidationAttribute
+{
+    private readonly EmailAddressAttribute _emailAddressAttribute = new();
+
+    public EmailConstraintAttribute()
+    {
+        ErrorMessage = "Invalid email address";
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        var strValue = value as string;
+
+        if (string.IsNullOrWhiteSpace(strValue))
+            return new ValidationResult("Email cannot be empty");
+
+        if (strValue.Length < 5 || strValue.Length > 150)
+            return new ValidationResult("Email must be between 5 and 150 characters");
+
+        if (!_emailAddressAttribute.IsValid(value))
+            return new ValidationResult("Invalid email format");
+        
+
+        return ValidationResult.Success;
+    }
+}
