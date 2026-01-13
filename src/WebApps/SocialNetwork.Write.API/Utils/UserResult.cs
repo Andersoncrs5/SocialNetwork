eab2f2aca29fab6efefb4dbb87ae9982f2b258ed
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-13 20:29:38 -0300

    New class UserResult to help the handler with result of db

---
+++ src/WebApps/SocialNetwork.Write.API/Utils/UserResult.cs
@@ -0,0 +1,10 @@
+using SocialNetwork.Write.API.Models;
+
+namespace SocialNetwork.Write.API.Utils;
+
+public class UserResult
+{
+    public bool Succeeded { get; set; }
+    public UserModel? User { get; set; } 
+    public IEnumerable<string>? Errors { get; set; } = new List<string>();
+}
