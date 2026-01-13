de01b6456df1f335f2aa7635ac228b3e449b9931
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-13 19:49:14 -0300

    Impled RoleRepository with interface and provider

---
+++ src/WebApps/SocialNetwork.Write.API/Repositories/Interfaces/IRoleRepository.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Identity;
+using SocialNetwork.Write.API.Models;
+
+namespace SocialNetwork.Write.API.Repositories.Interfaces;
+
+public interface IRoleRepository
+{
+    Task<IEnumerable<RoleModel>> GetAllAsync();
+    Task<RoleModel?> GetByIdAsync(string id);
+    Task<bool> ExistsByIdAsync(string id);
+    Task<RoleModel?> GetByNameAsync(string name);
+    Task<bool> ExistsByNameAsync(string name);
+    Task<IdentityResult> CreateAsync(RoleModel role);
+    Task<IdentityResult> UpdateAsync(RoleModel role);
+    Task<IdentityResult> DeleteAsync(RoleModel role);
+}
