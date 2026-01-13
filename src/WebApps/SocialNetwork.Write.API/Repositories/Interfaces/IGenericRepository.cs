2ca2a07927ddf6f80e73863377dc06d25ad47402
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-13 14:47:34 -0300

    Impled GenericRepository with any methods generics

---
+++ src/WebApps/SocialNetwork.Write.API/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,25 @@
+using System.Linq.Expressions;
+
+namespace SocialNetwork.Write.API.Repositories.Interfaces;
+
+public interface IGenericRepository<T> where T: class
+{
+    Task<T?> GetByIdInCache(string id, TimeSpan? cacheExpiry = null);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<bool> ExistsById(string Id);
+    Task<int> CountByIdAsync(string Id);
+    IQueryable<T> Query();
+    Task<T?> GetByIdAsync(string id);
+    Task<T> AddAsync(T entity);
+    Task AddRangeAsync(IEnumerable<T> entities);
+    Task<T> Update(T entity);
+    Task UpdateRangeAsync(IEnumerable<T> entities);
+    void Delete(T entity);
+    Task DeleteAsync(T entity);
+    Task DeleteRangeAsync(IEnumerable<T> entities);
+    Task SaveChangesAsync();
+    Task<T?> GetSingleOrDefaultAsync(Expression<Func<T, bool>> predicate);
+    Task<IEnumerable<T>> GetWhereAsync(Expression<Func<T, bool>> predicate);
+    Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null);
+    
+}
---
+++ src/WebApps/SocialNetwork.Write.API/Repositories/Provider/GenericRepository.cs
@@ -0,0 +1,127 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using SocialNetwork.Write.API.Configs.DB;
+using SocialNetwork.Write.API.Models.Bases;
+using SocialNetwork.Write.API.Repositories.Interfaces;
+using SocialNetwork.Write.API.Services.Interfaces;
+
+namespace SocialNetwork.Write.API.Repositories.Provider;
+
+public class GenericRepository<T>: IGenericRepository<T> where T : BaseModel
+{
+    protected readonly AppDbContext Context;
+    private readonly DbSet<T> _dbSet;
+    private readonly IRedisService _redisService;
+    
+    protected GenericRepository(AppDbContext ctx, IRedisService redisService)
+    {
+        Context = ctx;
+        _dbSet = Context.Set<T>();
+        _redisService = redisService;
+    }
+
+    public async Task<T?> GetByIdInCache(string id, TimeSpan? cacheExpiry = null)
+    {
+        T? inCache = await _redisService.GetAsync<T>(id);
+        
+        if (inCache != null)
+            return inCache;
+        
+        T? fromDb = await GetByIdAsync(id);
+        
+        if (fromDb != null)
+            await _redisService.CreateAsync(id, fromDb, cacheExpiry);
+        
+        return fromDb;
+    }
+
+    public async Task<bool> ExistsById(string Id)
+    {
+        return await _dbSet
+            .AnyAsync(c => c.Id == Id);
+    }
+    
+    public async Task<int> CountByIdAsync(string Id)
+    {
+        return await _dbSet
+            .CountAsync(c => c.Id == Id);
+    }
+    
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await this._dbSet.ToListAsync();
+    }
+    
+    public IQueryable<T> Query()
+    {
+        return this._dbSet;
+    }
+
+    public async Task<T?> GetByIdAsync(string id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<T> AddAsync(T entity)
+    {
+        entity.CreatedAt = DateTime.UtcNow;
+        EntityEntry<T> data = await _dbSet.AddAsync(entity);
+        return data.Entity;
+    }
+
+    public async Task AddRangeAsync(IEnumerable<T> entities)
+    {
+        await _dbSet.AddRangeAsync(entities);
+    }
+    
+    public async Task<T> Update(T entity)
+    {
+        EntityEntry<T> update = _dbSet.Update(entity);
+        return update.Entity;
+    }
+
+    public async Task UpdateRangeAsync(IEnumerable<T> entities)
+    {
+        await Task.Run(() => _dbSet.UpdateRange(entities));
+    }
+    
+    public void Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+    }
+
+    public async Task DeleteAsync(T entity)
+    {
+        await Task.Run(() => _dbSet.Remove(entity));
+    }
+
+    public async Task DeleteRangeAsync(IEnumerable<T> entities)
+    {
+        await Task.Run(() => _dbSet.RemoveRange(entities));
+    }
+    
+    public async Task SaveChangesAsync()
+    {
+        await Context.SaveChangesAsync();
+    }
+    
+    public async Task<T?> GetSingleOrDefaultAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.SingleOrDefaultAsync(predicate);
+    }
+    
+    public async Task<IEnumerable<T>> GetWhereAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.Where(predicate).ToListAsync();
+    }
+
+    public async Task<int> CountAsync(Expression<Func<T, bool>>? predicate = null)
+    {
+        if (predicate == null)
+            return await _dbSet.CountAsync();
+            
+        return await _dbSet.CountAsync(predicate);
+    }
+    
+}
