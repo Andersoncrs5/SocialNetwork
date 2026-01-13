870f1b167216c51174aaa97be4c380b66f1ae77a
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-13 20:28:00 -0300

    Set up base to futures dtos

---
+++ src/BuildingBlocks/SocialNetwork.Contracts/DTOs/BaseDto.cs
@@ -0,0 +1,8 @@
+namespace SocialNetwork.Contracts.DTOs;
+
+public class BaseDto
+{
+    public required string Id { get; set; } 
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
