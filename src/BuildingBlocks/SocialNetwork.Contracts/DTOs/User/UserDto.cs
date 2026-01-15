7e8cd7517105559a13618d270f98e398d4147535
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-15 20:03:15 -0300

    New columns

--- src/BuildingBlocks/SocialNetwork.Contracts/DTOs/User/UserDto.cs
+++ src/BuildingBlocks/SocialNetwork.Contracts/DTOs/User/UserDto.cs
@@ -5,4 +5,11 @@ public class UserDto: BaseDto
     public string? Email { get; set; }
     public string? Username { get; set; }
     public string? FullName { get; set; }
+    public string? Bio { get; set; } 
+    public string? CoverImageUrl { get; set; }
+    public DateTime? BirthDate { get; set; }
+    public bool IsPrivate { get; set; }  
+    public string? Language { get; set; }    
+    public string? Country { get; set; }    
+    public string? ImageProfileUrl { get; set; }
 }
--- src/WebApps/SocialNetwork.Write.API/Models/UserModel.cs
+++ src/WebApps/SocialNetwork.Write.API/Models/UserModel.cs
@@ -5,9 +5,21 @@ namespace SocialNetwork.Write.API.Models;
 public class UserModel: IdentityUser
 {
     public string? FullName { get; set; }
+    
+    public string? Bio { get; set; } 
+    public string? CoverImageUrl { get; set; }
+    public DateTime? BirthDate { get; set; }
+    public bool IsPrivate { get; set; }  
+    public string? Language { get; set; }    
+    public string? Country { get; set; }    
+    
     public string? ImageProfileUrl { get; set; }
     public string? RefreshToken { get; set; }
     public DateTime? RefreshTokenExpiryTime { get; set; }
+    
+    public int FailedLoginAttempts { get; set; }
+    public DateTime? LockoutEnd { get; set; }
+    
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
 }
