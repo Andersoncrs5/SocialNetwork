37ec9f897a90b26239e9c69a6eff0a3b356d0775
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-17 17:47:08 -0300

    Set up UniqueEmailAttribute

---
+++ src/WebApps/SocialNetwork.Write.API/Utils/Valids/Annotations/User/UniqueEmailAttribute.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using SocialNetwork.Write.API.Repositories.Interfaces;
+
+namespace SocialNetwork.Write.API.Utils.Valids.Annotations.User;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
+public class UniqueEmailAttribute: ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is not string email || string.IsNullOrWhiteSpace(email))
+            return new ValidationResult("Email is required");
+        
+        IUserRepository userRepository = (IUserRepository)validationContext.GetRequiredService(typeof(IUserRepository));
+        
+        var exists = userRepository.ExistsByEmail(email).GetAwaiter().GetResult();
+        
+        if (exists)
+            return new ValidationResult(ErrorMessage ?? "Email already exists");
+        
+        return ValidationResult.Success;
+    }
+}
