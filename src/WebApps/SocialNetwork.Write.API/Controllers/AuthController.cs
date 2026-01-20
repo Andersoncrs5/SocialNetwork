507345905df740bc96a0b944fae9a1117cbe4c1b
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-20 11:27:17 -0300

    Added logic to procter of force login

--- src/WebApps/SocialNetwork.Write.API/Controllers/AuthController.cs
+++ src/WebApps/SocialNetwork.Write.API/Controllers/AuthController.cs
@@ -48,6 +48,11 @@ public class AuthController(
 
         ResponseTokens tokens = tokenService.CreateTokens(user, roles);
         
+        user.RefreshToken = tokens.RefreshToken;
+        user.RefreshTokenExpiryTime = tokens.ExpiredAtRefreshToken;
+        
+        await userService.UpdateSimple(user);
+        
         return Ok(new ResponseHttp<ResponseTokens>(
             Data: tokens,
             ErrorCode: 0,
@@ -64,30 +69,50 @@ public class AuthController(
     [ProducesResponseType(typeof(ResponseHttp<ResponseTokens>), (int)HttpStatusCode.OK)]
     public async Task<IActionResult> Login([FromBody] LoginUserDto dto)
     {
-        UserModel? user = await userService.GetUserByEmail(dto.Email);
+        var user = await userService.GetUserByEmail(dto.Email);
+        var traceId = HttpContext.TraceIdentifier;
 
-        if (user is null)
-            return Unauthorized();
+         var invalidCredentialsResponse = Unauthorized(new ResponseHttp<object>(
+            null, "Invalid credentials", traceId, 1, false, DateTime.UtcNow));
 
-        bool checkPassword = await userService.CheckPassword(user, dto.Password);
+        if (user is null) return invalidCredentialsResponse;
+
+        if (user.LockoutEnd > DateTime.UtcNow)
+        {
+            return StatusCode(423, new ResponseHttp<object>( 
+                null, $"Account locked until {user.LockoutEnd}", traceId, 2, false, DateTime.UtcNow));
+        }
+
+        bool isPasswordValid = await userService.CheckPassword(user, dto.Password);
+
+        if (!isPasswordValid)
+        {
+            user.FailedLoginAttempts += 1;
         
-        if (!checkPassword)
-            return Unauthorized();
+            if (user.FailedLoginAttempts >= 3)
+            {
+                user.LockoutEnd = DateTime.UtcNow.AddHours(5);
+                user.FailedLoginAttempts = 0; 
+            }
+        
+            await userService.UpdateSimple(user);
+            return invalidCredentialsResponse;
+        }
 
-        IList<string> roles = await userService.GetUserRoles(user);
+        user.FailedLoginAttempts = 0;
+        user.LockoutEnd = null;
+    
+        var roles = await userService.GetUserRoles(user);
+        var tokens = tokenService.CreateTokens(user, roles);
+    
+        user.RefreshToken = tokens.RefreshToken;
+        user.RefreshTokenExpiryTime = tokens.ExpiredAtRefreshToken;
+    
+        await userService.UpdateSimple(user);
 
-        ResponseTokens tokens = tokenService.CreateTokens(user, roles);
-
-        ResponseHttp<ResponseTokens> response = new ResponseHttp<ResponseTokens>(
-            Data: tokens,
-            ErrorCode: 0,
-            Message: "Login success",
-            Success: true,
-            Timestamp: DateTime.UtcNow,
-            TraceId: HttpContext.TraceIdentifier
-        );
-
-        return Ok(response);
+        return Ok(new ResponseHttp<ResponseTokens>(tokens, "Login success", traceId, 0, true, DateTime.UtcNow));
     }
+    
+    
     
 }
