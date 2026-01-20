b934ad3cba99e869ee9e8d4776124fe3d86b9583
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-20 13:48:40 -0300

    Added and configured RedisContainer

--- tests/SocialNetwork.Write.IntegrationTests/Config/WriteApiFactory.cs
+++ tests/SocialNetwork.Write.IntegrationTests/Config/WriteApiFactory.cs
@@ -4,7 +4,9 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SocialNetwork.Write.API.Configs.DB;
+using StackExchange.Redis;
 using Testcontainers.MySql;
+using Testcontainers.Redis;
 
 namespace SocialNetwork.Write.IntegrationTests.Config;
 
@@ -16,6 +18,10 @@ public class WriteApiFactory : WebApplicationFactory<Program>, IAsyncLifetime
         .WithUsername("root")
         .WithPassword("123456")
         .Build();
+    
+    private readonly RedisContainer _redisContainer = new RedisBuilder()
+        .WithImage("redis:latest")
+        .Build();
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
@@ -26,12 +32,21 @@ public class WriteApiFactory : WebApplicationFactory<Program>, IAsyncLifetime
             var descriptor = services.SingleOrDefault(
                 d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
             if (descriptor != null) services.Remove(descriptor);
+            
+            var redisDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(IConnectionMultiplexer));
+            if (redisDescriptor != null) services.Remove(redisDescriptor);
 
+            var redisConn = _redisContainer.GetConnectionString();
+            var multiplexer = ConnectionMultiplexer.Connect(redisConn);
+            
+            services.AddSingleton<IConnectionMultiplexer>(multiplexer);
+            services.AddScoped<IDatabase>(s => multiplexer.GetDatabase());
+            
             var connectionString = _dbContainer.GetConnectionString();
 
             services.AddDbContext<AppDbContext>(options =>
             {
-                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+                options.UseMySQL(connectionString);
             });
 
             using var scope = services.BuildServiceProvider().CreateScope();
@@ -42,11 +57,17 @@ public class WriteApiFactory : WebApplicationFactory<Program>, IAsyncLifetime
 
     public async Task InitializeAsync()
     {
-        await _dbContainer.StartAsync();
+        await Task.WhenAll(
+            _dbContainer.StartAsync(), 
+            _redisContainer.StartAsync()
+        );
     }
 
     public new async Task DisposeAsync()
     {
-        await _dbContainer.StopAsync();
+        await Task.WhenAll(
+            _dbContainer.StopAsync(),
+            _redisContainer.StopAsync()
+        );
     }
 }
