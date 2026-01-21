f8437e366457d9c41790a037c2f61308286a8388
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-20 21:37:27 -0300

    Added InfoAppOptions

--- src/WebApps/SocialNetwork.Write.API/Services/Providers/TokenService.cs
+++ src/WebApps/SocialNetwork.Write.API/Services/Providers/TokenService.cs
@@ -9,20 +9,26 @@ using Microsoft.IdentityModel.Tokens;
 using Renci.SshNet.Security;
 using SocialNetwork.Contracts.configs.jwt;
 using SocialNetwork.Contracts.Utils.Res.http;
+using SocialNetwork.Write.API.Configs.InfoApp;
 using SocialNetwork.Write.API.Models;
 using SocialNetwork.Write.API.Services.Interfaces;
 
 namespace SocialNetwork.Write.API.Services.Providers;
 
-public class TokenService(IOptions<JwtOptions> jwtOptions) : ITokenService
+public class TokenService(
+    IOptions<JwtOptions> jwtOptions,
+    IOptions<InfoAppOptions> infoAppOptions
+    ) : ITokenService
 {
     private readonly JwtOptions _options = jwtOptions.Value;
+    private readonly InfoAppOptions _appOptions = infoAppOptions.Value;
+    private readonly string _alg = SecurityAlgorithms.HmacSha256;
 
     public string GenerateAccessToken(IEnumerable<Claim> claims)
     {
         var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey));
-        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
+        var creds = new SigningCredentials(key, _alg);
+        
         var token = new JwtSecurityToken(
             issuer: _options.ValidIssuer,
             audience: _options.ValidAudience,
@@ -74,6 +80,8 @@ public class TokenService(IOptions<JwtOptions> jwtOptions) : ITokenService
             new Claim(JwtRegisteredClaimNames.Name, user.UserName!),
             new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
             new Claim(JwtRegisteredClaimNames.Sid, user.Id),
+            new Claim(JwtRegisteredClaimNames.Iss, _appOptions.Domain),
+            new Claim(JwtRegisteredClaimNames.Aud, _options.ValidAudience),
             new Claim("country", user.Country ?? string.Empty),
             new Claim("language", user.Language ?? string.Empty),
             new Claim("lockoutEnd", user.LockoutEnd.ToString() ?? string.Empty),
