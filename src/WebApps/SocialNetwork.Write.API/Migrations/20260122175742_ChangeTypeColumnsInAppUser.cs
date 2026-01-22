af8740280bb8d84519bf6915ea2de3a3336446a1
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-22 20:51:52 -0300

    new migrations

---
+++ src/WebApps/SocialNetwork.Write.API/Migrations/20260122175742_ChangeTypeColumnsInAppUser.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialNetwork.Write.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTypeColumnsInAppUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageProfileUrl",
+                table: "app_users",
+                type: "VARCHAR(800)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(800)",
+                oldMaxLength: 800,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CoverImageUrl",
+                table: "app_users",
+                type: "VARCHAR(800)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(800)",
+                oldMaxLength: 800,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageProfileUrl",
+                table: "app_users",
+                type: "varchar(800)",
+                maxLength: 800,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(800)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CoverImageUrl",
+                table: "app_users",
+                type: "varchar(800)",
+                maxLength: 800,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(800)",
+                oldNullable: true);
+        }
+    }
+}
