c79d78bacda54eb0b9e100e7d5c9e5517a44ae5b
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-31 17:35:36 -0300

    new endpoints

--- src/WebApps/SocialNetwork.Write.API/Controllers/CategoryController.cs
+++ src/WebApps/SocialNetwork.Write.API/Controllers/CategoryController.cs
@@ -4,6 +4,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Contracts.Attributes.Globals;
 using SocialNetwork.Contracts.DTOs.User;
 using SocialNetwork.Contracts.Utils.Res.http;
 using SocialNetwork.Write.API.dto.Category;
@@ -22,12 +23,12 @@ public class CategoryController(
     ) : Controller
 {
     [HttpPost]
-    [ProducesResponseType(typeof(ResponseHttp<object>), (int)HttpStatusCode.Unauthorized)]
+    [ProducesResponseType(typeof(ResponseHttp<object>), (int)HttpStatusCode.Forbidden)]
     [ProducesResponseType(typeof(ResponseHttp<CategoryDto>), (int)HttpStatusCode.Created)]
-    [ProducesResponseType(typeof(ResponseHttp<IEnumerable<string>>), (int)HttpStatusCode.OK)]
+    [ProducesResponseType(typeof(ResponseHttp<IDictionary<string, string[]>>), (int)HttpStatusCode.BadRequest)]
     [Authorize(Roles = "MASTER_ROLE, SUPER_ADM_ROLE")]
     public async Task<IActionResult> Create([FromBody] CreateCategoryDto dto)
-    {
+    { 
         CategoryModel model = await service.Create(dto);
         
         return StatusCode(StatusCodes.Status201Created, new ResponseHttp<CategoryDto>(
@@ -38,6 +39,45 @@ public class CategoryController(
             Timestamp: DateTime.UtcNow
         ));
     }
-    
+
+    [HttpDelete("{id:required}")]
+    [ProducesResponseType(typeof(ResponseHttp<object>), (int)HttpStatusCode.Forbidden)]
+    [ProducesResponseType(typeof(ResponseHttp<object>), (int)HttpStatusCode.OK)]
+    [ProducesResponseType(typeof(ResponseHttp<object>), (int)HttpStatusCode.NotFound)]
+    [Authorize(Roles = "MASTER_ROLE, SUPER_ADM_ROLE")]
+    public async Task<IActionResult> Delete([IsId] string id)
+    {
+        CategoryModel category = await service.GetByIdSimple(id);
+        await service.Delete(category);
+        
+        return StatusCode(StatusCodes.Status200OK, new ResponseHttp<object>(
+            Data: null,
+            Message:"Category deleted successfully",
+            TraceId: HttpContext.TraceIdentifier,
+            Success: true,
+            Timestamp: DateTime.UtcNow
+        ));
+    }
+
+    [HttpPatch("{id:required}")]
+    [ProducesResponseType(typeof(ResponseHttp<object>), (int)HttpStatusCode.Forbidden)]
+    [ProducesResponseType(typeof(ResponseHttp<CategoryDto>), (int)HttpStatusCode.OK)]
+    [ProducesResponseType(typeof(ResponseHttp<IDictionary<string, string[]>>), (int)HttpStatusCode.BadRequest)]
+    [Authorize(Roles = "MASTER_ROLE, SUPER_ADM_ROLE")]
+    public async Task<IActionResult> Patch([IsId] string id, [FromBody] UpdateCategoryDto dto)
+    {
+        CategoryModel category = await service.GetByIdSimple(id);
+
+        CategoryModel model = await service.Update(dto, category);
+
+        return StatusCode(StatusCodes.Status200OK, new ResponseHttp<CategoryDto>(
+            Data: mapper.Map<CategoryDto>(model),
+            Message:"Category updated successfully",
+            TraceId: HttpContext.TraceIdentifier,
+            Success: true,
+            Timestamp: DateTime.UtcNow
+        ));
+    }
+
     
 }
