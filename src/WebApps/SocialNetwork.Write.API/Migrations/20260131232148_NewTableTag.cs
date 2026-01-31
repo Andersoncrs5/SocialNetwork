9aeee51587b3bf2796fa2fe67ca6cd42e82e0ecb
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-31 20:26:14 -0300

    new migrations

---
+++ src/WebApps/SocialNetwork.Write.API/Migrations/20260131232148_NewTableTag.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialNetwork.Write.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewTableTag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(450)", maxLength: 450, nullable: false),
+                    Name = table.Column<string>(type: "VARCHAR(150)", maxLength: 150, nullable: false),
+                    Slug = table.Column<string>(type: "VARCHAR(250)", maxLength: 250, nullable: false),
+                    Description = table.Column<string>(type: "VARCHAR(500)", maxLength: 500, nullable: true),
+                    Color = table.Column<string>(type: "VARCHAR(10)", nullable: true),
+                    IsActive = table.Column<bool>(type: "tinyint(1)", nullable: false, defaultValue: true),
+                    IsVisible = table.Column<bool>(type: "tinyint(1)", nullable: false, defaultValue: true),
+                    IsSystem = table.Column<bool>(type: "tinyint(1)", nullable: false, defaultValue: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
