7514184952d17023a3ce58cb73e28f6ef34b4b6e
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-31 16:12:00 -0300

    new methods LoginMaster and GenerateChars

--- tests/SocialNetwork.Write.IntegrationTests/Tests/HelperTest.cs
+++ tests/SocialNetwork.Write.IntegrationTests/Tests/HelperTest.cs
@@ -11,6 +11,44 @@ namespace SocialNetwork.Write.IntegrationTests.Tests;
 
 public class HelperTest(HttpClient client)
 {
+    public async Task<UserTestResult> LoginMaster()
+    {
+        LoginUserDto dto = new LoginUserDto()
+        {
+            Email = "usersystem@example.com",
+            Password = "Aw53653%54Fe!",
+        };
+
+        HttpResponseMessage response = await client.PostAsJsonAsync("/api/v1/Auth/login", dto);
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        
+        ResponseHttp<ResponseTokens>? http = await response.Content.ReadFromJsonAsync<ResponseHttp<ResponseTokens>>();
+        
+        http.Should().NotBeNull();
+        http.Data.Should().NotBeNull();
+        http.Data.User.Should().NotBeNull();
+        
+        http.Data.Token.Should().NotBeNullOrEmpty();
+        http.Data.RefreshToken.Should().NotBeNullOrEmpty();
+        http.DetailsError.Should().BeNullOrWhiteSpace();
+        http.Success.Should().BeTrue();
+
+        CreateUserDto dtoRe = new CreateUserDto()
+        {
+            Email = http.Data.User.Email!,
+            Username = http.Data.User.Username!,
+            FullName = http.Data.User.FullName,
+            PasswordHash = dto.Password
+        };
+        
+        return new UserTestResult()
+        {
+            Dto = dtoRe ,
+            Tokens = http.Data,
+            User = http.Data.User,
+        };
+    }
+    
     public async Task<UserTestResult> CreateNewUser()
     {
         long num = Random.Shared.NextInt64(1, 10000000000000);
@@ -45,4 +83,17 @@ public class HelperTest(HttpClient client)
         };
 
     }
+
+    public string GenerateChars()
+    {
+        return string.Create(25, (object?)null, (buffer, _) =>
+        {
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                int asciiCode = Random.Shared.Next(65, 91); 
+                buffer[i] = (char)asciiCode;
+            }
+        });
+    }
+    
 }
