90afcba9c726396d57e71d34bcf3d5c04c5b27cf
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-31 20:06:12 -0300

    Added validations

--- src/WebApps/SocialNetwork.Write.API/dto/Category/UpdateCategoryDto.cs
+++ src/WebApps/SocialNetwork.Write.API/dto/Category/UpdateCategoryDto.cs
@@ -5,12 +5,19 @@ namespace SocialNetwork.Write.API.dto.Category;
 
 public class UpdateCategoryDto
 {
+    [MaxLength(150)]
     public string? Name { get; set; }
+    
+    [MaxLength(500)]
     public string? Description { get; set; }
+    
+    [MaxLength(800)]
     public string? IconName { get; set; }
     
     [MaxLength(7)]
     public string? Color { get; set; }
+    
+    [MaxLength(250)]
     public string? Slug { get; set; }
     public bool? IsActive { get; set; }
     public bool? IsVisible { get; set; }
