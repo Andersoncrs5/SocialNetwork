b0ba467d246dc14c1c018434390b380a28bccc18
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-01-31 20:10:35 -0300

    new migrations

---
+++ src/WebApps/SocialNetwork.Write.API/Migrations/20260131201525_IncreaseOnLimiteSizeToColumnColorInTableCategory.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialNetwork.Write.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseOnLimiteSizeToColumnColorInTableCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Color",
+                table: "Categories",
+                type: "VARCHAR(10)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(6)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Color",
+                table: "Categories",
+                type: "VARCHAR(6)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(10)",
+                oldNullable: true);
+        }
+    }
+}
