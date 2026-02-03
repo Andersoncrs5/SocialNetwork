8df81bb86450df878baee295b1da584d55c975ff
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-03 10:39:08 -0300

    Set up methods

--- src/WebApps/SocialNetwork.Write.API/Services/Interfaces/ITagService.cs
+++ src/WebApps/SocialNetwork.Write.API/Services/Interfaces/ITagService.cs
@@ -1,6 +1,13 @@
+using SocialNetwork.Contracts.Attributes.Globals;
+using SocialNetwork.Write.API.dto.Tag;
+using SocialNetwork.Write.API.Models;
+
 namespace SocialNetwork.Write.API.Services.Interfaces;
 
 public interface ITagService
 {
-    
+    Task<TagModel> GetByIdSimpleAsync([IsId] string id);
+    Task<bool> ExistsByIdAsync([IsId] string id);
+    Task<bool> ExistsByNameAsync(string name);
+    Task<TagModel> CreateAsync(CreateTagDto dto);
 }
--- src/WebApps/SocialNetwork.Write.API/Services/Providers/TagService.cs
+++ src/WebApps/SocialNetwork.Write.API/Services/Providers/TagService.cs
@@ -1,6 +1,68 @@
+using AutoMapper;
+using SocialNetwork.Contracts.Attributes.Globals;
+using SocialNetwork.Write.API.Configs.Exception.classes;
+using SocialNetwork.Write.API.dto.Tag;
+using SocialNetwork.Write.API.Models;
+using SocialNetwork.Write.API.Services.Interfaces;
+using SocialNetwork.Write.API.Utils.UnitOfWork;
+
 namespace SocialNetwork.Write.API.Services.Providers;
 
-public class TagService
+public class TagService(IUnitOfWork uow, IMapper mapper): ITagService
 {
+    public async Task<TagModel> GetByIdSimpleAsync([IsId] string id)
+        => await uow.TagRepository.GetByIdAsync(id) ?? throw new ModelNotFoundException("Tag not found");
+
+    public async Task<bool> ExistsByIdAsync([IsId] string id)
+        => await uow.TagRepository.ExistsById(id);
+
+    public async Task<bool> ExistsByNameAsync(string name)
+        => await uow.TagRepository.ExistsByName(name);
+
+    public async Task DeleteAsync(TagModel tag)
+    {
+        await uow.TagRepository.DeleteAsync(tag);
+        await uow.CommitAsync();
+    }
+
+    public async Task<TagModel> CreateAsync(CreateTagDto dto)
+    {
+        TagModel model = mapper.Map<TagModel>(dto);
+
+        TagModel tagCreated = await uow.TagRepository.AddAsync(model);
+        await uow.CommitAsync();
+        
+        return tagCreated;
+    }
+
+    public async Task<TagModel> UpdateAsync(TagModel tag, UpdateTagDto dto)
+    {
+        if (!string.IsNullOrWhiteSpace(dto.Name) && tag.Name != dto.Name)
+        {
+            if (await uow.TagRepository.ExistsByName(dto.Name))
+                throw new ConflictValueException($"Name: {dto.Name} already in use"); 
+            
+            tag.Name = dto.Name;
+        }
+        
+        if (!string.IsNullOrWhiteSpace(dto.Slug) && tag.Slug != dto.Slug)
+        {
+            if (await uow.TagRepository.ExistsBySlug(dto.Slug))
+                throw new ConflictValueException($"Slug: {dto.Slug} already in use"); 
+            
+            tag.Slug = dto.Slug;
+        }
+        
+        if (!string.IsNullOrWhiteSpace(dto.Description))  tag.Description = dto.Description;
+        if (!string.IsNullOrWhiteSpace(dto.Color))  tag.Color = dto.Color;
+        
+        if (dto.IsActive.HasValue) dto.IsActive = dto.IsActive.Value;
+        if (dto.IsVisible.HasValue) dto.IsVisible = dto.IsVisible.Value;
+        if (dto.IsSystem.HasValue) dto.IsSystem = dto.IsSystem.Value;
+        
+        TagModel update = await uow.TagRepository.Update(tag);
+        await uow.CommitAsync();
+        return update;
+    }
     
 }
