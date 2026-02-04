8eda3143610bff142f88c665dd03ea3d25ca6c59
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-03 21:39:21 -0300

    Set up ReadingLevelEnum

---
+++ src/WebApps/SocialNetwork.Write.API/Models/Enums/Post/ReadingLevelEnum.cs
@@ -0,0 +1,9 @@
+namespace SocialNetwork.Write.API.Models.Enums.Post;
+
+public enum ReadingLevelEnum
+{
+    VeryShort,   // < 2 min
+    Short,       // 2–5 min
+    Medium,      // 5–10 min
+    Long         // > 10 min
+}
