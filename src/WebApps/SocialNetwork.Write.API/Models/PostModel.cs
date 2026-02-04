2a6f1f7f9f605a0cf9974cc6e3ab195982ff56a4
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-03 21:38:56 -0300

    Set up PostModel

---
+++ src/WebApps/SocialNetwork.Write.API/Models/PostModel.cs
@@ -0,0 +1,21 @@
+using SocialNetwork.Write.API.Models.Bases;
+using SocialNetwork.Write.API.Models.Enums.Post;
+
+namespace SocialNetwork.Write.API.Models;
+
+public class PostModel: BaseModel
+{
+    public required string Title { get; set; }
+    public required string Slug { get; set; }
+    public required string Content { get; set; }
+    public string? Summary { get; set; }
+    public string? FeaturedImageUrl { get; set; }
+    public PostVisibilityEnum Visibility { get; set; }
+    public int? ReadingTime { get; set; }
+    public double RankingScore { get; set; }
+    public bool IsCommentsEnabled { get; set; }
+    public PostHighlightStatusEnum  HighlightStatus { get; set; }
+    public ModerationStatusEnum  ModerationStatus { get; set; }
+    public ReadingLevelEnum  ReadingLevel { get; set; }
+    public PostTypeEnum PostType { get; set; }
+}
