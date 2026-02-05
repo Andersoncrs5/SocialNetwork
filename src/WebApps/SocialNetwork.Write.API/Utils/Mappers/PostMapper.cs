668fa17634f0b69a4f09ecdfc4b3d0dfb32a21bb
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-05 14:54:35 -0300

    Set up PostMapper

---
+++ src/WebApps/SocialNetwork.Write.API/Utils/Mappers/PostMapper.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using SocialNetwork.Write.API.dto.Posts;
+using SocialNetwork.Write.API.Models;
+
+namespace SocialNetwork.Write.API.Utils.Mappers;
+
+public class PostMapper: Profile
+{
+    public PostMapper()
+    {
+        CreateMap<CreatePostDto, PostModel>();
+        CreateMap<PostModel, PostDto>().ReverseMap();
+    }
+}
