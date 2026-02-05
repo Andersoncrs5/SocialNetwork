b1cd277513312c8d19af6143ca9a0c959d3fe1a2
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-05 14:55:08 -0300

    Set up UpdatePostDto

---
+++ src/WebApps/SocialNetwork.Write.API/dto/Posts/UpdatePostDto.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using SocialNetwork.Write.API.Models.Enums.Post;
+
+namespace SocialNetwork.Write.API.dto.Posts;
+
+public class UpdatePostDto
+{
+    [MaxLength(150), MinLength(5)]
+    public string? Title { get; set; }
+    [MaxLength(250), MinLength(5)]
+    public string? Slug { get; set; }
+    
+    [MaxLength(700), MinLength(100)]
+    public string? Content { get; set; }
+    
+    [MaxLength(300)]
+    public string? Summary { get; set; }
+    
+    [MaxLength(800)]
+    public string? FeaturedImageUrl { get; set; }
+    public PostVisibilityEnum? Visibility { get; set; }
+    public int? ReadingTime { get; set; }
+    public bool? IsCommentsEnabled { get; set; }
+    public ReadingLevelEnum?  ReadingLevel { get; set; }
+    public PostTypeEnum? PostType { get; set; }
+}
