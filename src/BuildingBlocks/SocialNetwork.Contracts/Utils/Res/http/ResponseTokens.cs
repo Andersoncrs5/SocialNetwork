77b0c5482e6ceb42ec396beae623a27d14604eef
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-07 14:53:30 -0300

    New column Roles

--- src/BuildingBlocks/SocialNetwork.Contracts/Utils/Res/http/ResponseTokens.cs
+++ src/BuildingBlocks/SocialNetwork.Contracts/Utils/Res/http/ResponseTokens.cs
@@ -10,4 +10,5 @@ public class ResponseTokens
     public DateTime? ExpiredAt { get; set; }
     public DateTime? ExpiredAtRefreshToken { get; set; }
     public UserDto? User { get; set; }
+    public IList<string>? Roles { get; set; }
 }
