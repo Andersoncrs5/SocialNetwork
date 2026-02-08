cb3666da9fc39572bf1dce1b371dd8f8dc8a7a9b
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-07 21:24:45 -0300

    New migrations

---
+++ src/WebApps/SocialNetwork.Write.API/Migrations/20260207172548_NewColumnUserIDInTablePost.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialNetwork.Write.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewColumnUserIDInTablePost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Posts",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(700)",
+                oldMaxLength: 700);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Posts",
+                type: "VARCHAR(255)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_Slug",
+                table: "Posts",
+                column: "Slug",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_UserId",
+                table: "Posts",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_app_users_UserId",
+                table: "Posts",
+                column: "UserId",
+                principalTable: "app_users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_app_users_UserId",
+                table: "Posts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_Slug",
+                table: "Posts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_UserId",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "Posts",
+                type: "VARCHAR(700)",
+                maxLength: 700,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
