4ca69b4cc7760d58d28cd19a01a2c3b8a0a5537e
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-08 14:51:12 -0300

    Added more validations

--- src/WebApps/SocialNetwork.Write.API/dto/PostCategory/CreatePostCategoryDto.cs
+++ src/WebApps/SocialNetwork.Write.API/dto/PostCategory/CreatePostCategoryDto.cs
@@ -1,10 +1,12 @@
 using SocialNetwork.Contracts.Attributes.Globals;
+using SocialNetwork.Write.API.Utils.Valids.Annotations.Category;
+using SocialNetwork.Write.API.Utils.Valids.Annotations.Post;
 
 namespace SocialNetwork.Write.API.dto.PostCategory;
 
 public class CreatePostCategoryDto
 {
-    [IsId] public required string PostId { get; set; }
-    [IsId] public required string CategoryId { get; set; }
+    [IsId, ExistsPostId] public required string PostId { get; set; }
+    [IsId, ExistsCategoryById] public required string CategoryId { get; set; }
     public uint Order { get; set; }
 }
