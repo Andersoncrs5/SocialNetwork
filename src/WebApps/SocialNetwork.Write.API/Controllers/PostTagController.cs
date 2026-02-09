a629c900af581670e4c02447a47a026777791f6d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-09 20:23:35 -0300

    Set up PostTagController

---
+++ src/WebApps/SocialNetwork.Write.API/Controllers/PostTagController.cs
@@ -0,0 +1,101 @@
+using System.Net;
+using Asp.Versioning;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Contracts.DTOs.PostTag;
+using SocialNetwork.Contracts.Utils.Res.http;
+using SocialNetwork.Write.API.Configs.Exception.classes;
+using SocialNetwork.Write.API.dto.PostTag;
+using SocialNetwork.Write.API.Models;
+using SocialNetwork.Write.API.Services.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using SocialNetwork.Contracts.Attributes.Globals;
+using SocialNetwork.Contracts.Utils.Exceptions;
+
+namespace SocialNetwork.Write.API.Controllers;
+
+[ApiController]
+[Route("api/v{version:apiVersion}/post-tag")]
+[ApiVersion("1.0")]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+public class PostTagController(
+    IPostService postService,
+    ITagService tagService,
+    IUserService userService,
+    IPostTagService service,
+    IMapper mapper
+    ): Controller
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseHttp<object>), (int)HttpStatusCode.Forbidden)]
+    [ProducesResponseType(typeof(ResponseHttp<object>), (int)HttpStatusCode.BadRequest)]
+    [ProducesResponseType(typeof(ResponseHttp<object>), (int)HttpStatusCode.NotFound)]
+    [ProducesResponseType(typeof(ResponseHttp<PostTagDto>), (int)HttpStatusCode.Created)]
+    [ProducesResponseType(typeof(ResponseHttp<IDictionary<string, string[]>>), (int)HttpStatusCode.BadRequest)]
+    [Authorize(Roles = "USER_ROLE")]
+    public async Task<IActionResult> Create(CreatePostTagDto dto)
+    {
+        string userId = User.FindFirst(JwtRegisteredClaimNames.Sid)?.Value ?? throw new UnauthenticatedException();
+        
+        PostModel post = await postService.GetByIdSimpleAsync(dto.PostId);
+        await tagService.GetByIdSimpleAsync(dto.TagId);
+        
+        if (post.UserId != userId)
+            throw new ResourceOwnerMismatchException("You don't have permission to add tag this post");
+        
+        int result = await service.CountByPostIdAndTagId(dto.PostId, dto.TagId);
+
+        if (result >= 10) 
+        {
+            return StatusCode(StatusCodes.Status400BadRequest, new ResponseHttp<object>(
+                Data: null,
+                Message:"Limit of 10 by post",
+                TraceId: HttpContext.TraceIdentifier,
+                Success: false,
+                Timestamp: DateTime.UtcNow
+            ));
+        }
+        
+        PostTagModel tagAdded = await service.Create(dto);
+        
+        return StatusCode(StatusCodes.Status201Created, new ResponseHttp<PostTagDto>(
+            Data: mapper.Map<PostTagDto>(tagAdded),
+            Message:"Tag added successfully",
+            TraceId: HttpContext.TraceIdentifier,
+            Success: true,
+            Timestamp: DateTime.UtcNow
+        ));
+    }
+
+    [HttpDelete("{id:required}")]
+    [ProducesResponseType(typeof(ResponseHttp<object>), (int)HttpStatusCode.OK)]
+    [ProducesResponseType(typeof(ResponseHttp<object>), (int)HttpStatusCode.BadRequest)]
+    [ProducesResponseType(typeof(ResponseHttp<object>), (int)HttpStatusCode.Forbidden)]
+    [ProducesResponseType(typeof(ResponseHttp<object>), (int)HttpStatusCode.NotFound)]
+    [Authorize(Roles = "USER_ROLE")]
+    public async Task<IActionResult> Delete([IsId] string id)
+    {
+        string userId = User.FindFirst(JwtRegisteredClaimNames.Sid)?.Value ?? throw new UnauthenticatedException();
+
+        PostTagModel model = await service.GetByIdAsync(id);
+        
+        PostModel post = await postService.GetByIdSimpleAsync(model.PostId);
+        
+        if (post.UserId != userId)
+            throw new ResourceOwnerMismatchException("You don't have permission to add tag this post");
+
+        await service.Delete(model);
+        
+        return StatusCode(StatusCodes.Status200OK, new ResponseHttp<PostTagDto>(
+            Data: null,
+            Message:"Tag removed successfully",
+            TraceId: HttpContext.TraceIdentifier,
+            Success: true,
+            Timestamp: DateTime.UtcNow
+        ));
+    }
+    
+    
+}
