7802302b5d89b0d740ddf5190b1b57e9a1d93378
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-09 20:54:14 -0300

    New enum LanguageEnum

---
+++ src/BuildingBlocks/SocialNetwork.Contracts/Utils/Enums/LanguageEnum.cs
@@ -0,0 +1,18 @@
+namespace SocialNetwork.Contracts.Utils.Enums;
+
+public enum LanguageEnum
+{
+    Undefined = 0,
+    Portuguese = 1,
+    English = 2,   
+    Spanish = 3,        
+    French = 4,         
+    German = 5,         
+    Italian = 6,        
+    Japanese = 7,       
+    Korean = 8,         
+    Chinese = 9,        
+    Russian = 10,       
+    Arabic = 11,        
+    Hindi = 12          
+}
