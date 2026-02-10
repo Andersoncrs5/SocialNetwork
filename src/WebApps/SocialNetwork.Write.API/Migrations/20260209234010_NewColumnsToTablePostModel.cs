666d1130068f96b9afbd98c18f27c8819e497e58
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-09 21:14:12 -0300

    New migrations

---
+++ src/WebApps/SocialNetwork.Write.API/Migrations/20260209234010_NewColumnsToTablePostModel.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialNetwork.Write.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewColumnsToTablePostModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "EstimatedValue",
+                table: "Posts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Language",
+                table: "Posts",
+                type: "varchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "Undefined");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ParentId",
+                table: "Posts",
+                type: "varchar(450)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Pinned",
+                table: "Posts",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_ParentId",
+                table: "Posts",
+                column: "ParentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Posts_ParentId",
+                table: "Posts",
+                column: "ParentId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Posts_ParentId",
+                table: "Posts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Posts_ParentId",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "EstimatedValue",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "ParentId",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Pinned",
+                table: "Posts");
+        }
+    }
+}
---
+++ src/WebApps/SocialNetwork.Write.API/Migrations/20260209234940_FixErrorInTablePostModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialNetwork.Write.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixErrorInTablePostModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
