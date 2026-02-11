46cdd20e407d45a7ccea5dd9c5afa8edc5c55cb8
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-11 09:25:55 -0300

    Added CommentModel to AppDbContext

--- src/WebApps/SocialNetwork.Write.API/Migrations/20260211114315_AddedTableCommentInAppDbContext.Designer.cs
+++ src/WebApps/SocialNetwork.Write.API/Migrations/20260211114315_AddedTableCommentInAppDbContext.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SocialNetwork.Write.API.Configs.DB;
 
@@ -10,9 +11,11 @@ using SocialNetwork.Write.API.Configs.DB;
 namespace SocialNetwork.Write.API.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260211114315_AddedTableCommentInAppDbContext")]
+    partial class AddedTableCommentInAppDbContext
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/WebApps/SocialNetwork.Write.API/Migrations/20260211114315_AddedTableCommentInAppDbContext.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialNetwork.Write.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTableCommentInAppDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CommentModel_CommentModel_ParentId",
+                table: "CommentModel");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CommentModel_Posts_PostId",
+                table: "CommentModel");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CommentModel_app_users_UserId",
+                table: "CommentModel");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CommentModel",
+                table: "CommentModel");
+
+            migrationBuilder.RenameTable(
+                name: "CommentModel",
+                newName: "Comments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CommentModel_UserId",
+                table: "Comments",
+                newName: "IX_Comments_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CommentModel_PostId",
+                table: "Comments",
+                newName: "IX_Comments_PostId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CommentModel_ParentId",
+                table: "Comments",
+                newName: "IX_Comments_ParentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Comments_ParentId",
+                table: "Comments",
+                column: "ParentId",
+                principalTable: "Comments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_app_users_UserId",
+                table: "Comments",
+                column: "UserId",
+                principalTable: "app_users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Comments_ParentId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_app_users_UserId",
+                table: "Comments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments");
+
+            migrationBuilder.RenameTable(
+                name: "Comments",
+                newName: "CommentModel");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_UserId",
+                table: "CommentModel",
+                newName: "IX_CommentModel_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_PostId",
+                table: "CommentModel",
+                newName: "IX_CommentModel_PostId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_ParentId",
+                table: "CommentModel",
+                newName: "IX_CommentModel_ParentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CommentModel",
+                table: "CommentModel",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CommentModel_CommentModel_ParentId",
+                table: "CommentModel",
+                column: "ParentId",
+                principalTable: "CommentModel",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CommentModel_Posts_PostId",
+                table: "CommentModel",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CommentModel_app_users_UserId",
+                table: "CommentModel",
+                column: "UserId",
+                principalTable: "app_users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
