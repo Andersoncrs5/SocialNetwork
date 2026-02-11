3511f2674ad04c80ddff1ea3c8c474d2254b86c4
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-11 09:26:35 -0300

    Added Validation to column PostId

--- src/WebApps/SocialNetwork.Write.API/dto/Comment/CreateCommentDto.cs
+++ src/WebApps/SocialNetwork.Write.API/dto/Comment/CreateCommentDto.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using SocialNetwork.Contracts.Attributes.Globals;
 using SocialNetwork.Write.API.Utils.Valids.Annotations.Comment;
+using SocialNetwork.Write.API.Utils.Valids.Annotations.Post;
 
 namespace SocialNetwork.Write.API.dto.Comment;
 
@@ -8,6 +9,6 @@ public class CreateCommentDto
 {
     [MaxLength(400), Required, MinLength(1)]
     public required string Content { get; set; }
-    [IsId] public required string PostId { get; set; }
+    [IsId, ExistsPostId] public required string PostId { get; set; }
     [ExistsCommentById] public string? ParentId { get; set; }
 }
