75cd6d236bc6eb89beb210357844e55bd76dbfb0
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-10 21:05:57 -0300

    Set up CommentDto

---
+++ src/BuildingBlocks/SocialNetwork.Contracts/DTOs/Comment/CommentDto.cs
@@ -0,0 +1,19 @@
+using SocialNetwork.Contracts.DTOs.User;
+using SocialNetwork.Write.API.Models.Enums.Post;
+
+namespace SocialNetwork.Contracts.DTOs.Comment;
+
+public class CommentDto: BaseDto
+{
+    public required string Content { get; set; }
+    public double SentimentScore { get; set; }
+    public bool IsEdited { get; set; }
+    public ModerationStatusEnum ModerationStatus { get; set; }
+     
+    public required string PostId { get; set; }
+    
+    public required string UserId { get; set; }
+    public UserDto? User { get; set; }
+    
+    public string? ParentId { get; set; }
+}
