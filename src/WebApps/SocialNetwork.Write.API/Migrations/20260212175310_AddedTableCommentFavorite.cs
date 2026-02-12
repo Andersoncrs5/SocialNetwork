a0787d6279c17b71cb4f9b846eb3ed8d766f10f3
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-12 18:19:16 -0300

    New migrations

---
+++ src/WebApps/SocialNetwork.Write.API/Migrations/20260212175310_AddedTableCommentFavorite.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialNetwork.Write.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTableCommentFavorite : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CommentFavorites",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(450)", maxLength: 450, nullable: false),
+                    CommentId = table.Column<string>(type: "varchar(450)", nullable: false),
+                    UserId = table.Column<string>(type: "varchar(255)", nullable: false),
+                    CommentModelId = table.Column<string>(type: "varchar(450)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CommentFavorites", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CommentFavorites_Comments_CommentId",
+                        column: x => x.CommentId,
+                        principalTable: "Comments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CommentFavorites_Comments_CommentModelId",
+                        column: x => x.CommentModelId,
+                        principalTable: "Comments",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_CommentFavorites_app_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "app_users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommentFavorites_CommentId",
+                table: "CommentFavorites",
+                column: "CommentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommentFavorites_CommentModelId",
+                table: "CommentFavorites",
+                column: "CommentModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CommentFavorites_UserId_CommentId",
+                table: "CommentFavorites",
+                columns: new[] { "UserId", "CommentId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CommentFavorites");
+        }
+    }
+}
