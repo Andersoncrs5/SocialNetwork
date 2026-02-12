024926d60f33a263e558152f9baed636a4340f39
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-12 14:47:12 -0300

    New migrations

---
+++ src/WebApps/SocialNetwork.Write.API/Migrations/20260211151314_AddedTablePostFavorites.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialNetwork.Write.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTablePostFavorites : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PostFavorites",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(450)", maxLength: 450, nullable: false),
+                    PostId = table.Column<string>(type: "varchar(450)", nullable: false),
+                    UserId = table.Column<string>(type: "varchar(255)", nullable: false),
+                    PostModelId = table.Column<string>(type: "varchar(450)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PostFavorites", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PostFavorites_Posts_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Posts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PostFavorites_Posts_PostModelId",
+                        column: x => x.PostModelId,
+                        principalTable: "Posts",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_PostFavorites_app_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "app_users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostFavorites_PostId",
+                table: "PostFavorites",
+                column: "PostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostFavorites_PostModelId",
+                table: "PostFavorites",
+                column: "PostModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PostFavorites_UserId_PostId",
+                table: "PostFavorites",
+                columns: new[] { "UserId", "PostId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PostFavorites");
+        }
+    }
+}
