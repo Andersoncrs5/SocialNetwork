1042556fdc79b24b6c4330bc36740c49ea029c2f
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-02-17 22:23:02 -0300

    Set up UpdateReactionDto

---
+++ src/WebApps/SocialNetwork.Write.API/dto/Reaction/UpdateReactionDto.cs
@@ -0,0 +1,14 @@
+using SocialNetwork.Contracts.Enums.Reaction;
+
+namespace SocialNetwork.Write.API.dto.Reaction;
+
+public class UpdateReactionDto
+{
+    public string? Name { get; set; }
+    public ReactionTypeEnum? Type { get; set; }
+    public string? EmojiUrl { get; set; }
+    public string? EmojiUnicode { get; set; }
+    public long? DisplayOrder { get; set; }
+    public bool? Active { get; set; }
+    public bool? Visible { get; set; }
+}
