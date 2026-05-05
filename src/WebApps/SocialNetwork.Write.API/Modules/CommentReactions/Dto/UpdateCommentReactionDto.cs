df9136521d3ddc6ffffcc90959212a4d27b0f86f
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-05 14:35:21 -0300

    Set up UpdateCommentReactionDto

---
+++ src/WebApps/SocialNetwork.Write.API/Modules/CommentReactions/Dto/UpdateCommentReactionDto.cs
@@ -0,0 +1,8 @@
+using SocialNetwork.Contracts.Attributes.Globals;
+
+namespace SocialNetwork.Write.API.Modules.CommentReactions.Dto;
+
+public class UpdateCommentReactionDto
+{
+    public string? ReactionId { get; set; }
+}
