4390f9bbfc2440a29098df6d096a6f9d7922a746
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-06 08:45:47 -0300

    improvement in method IsForeignKeyViolation

--- src/WebApps/SocialNetwork.Write.API/Configs/DB/DbErrorHelper.cs
+++ src/WebApps/SocialNetwork.Write.API/Configs/DB/DbErrorHelper.cs
@@ -8,15 +8,30 @@ public static class DbErrorHelper
     private const int ForeignKeyNoParentCode = 1452; 
     private const int ForeignKeyHasChildrenCode = 1451;
     private const int DataTooLongCode = 1406;
+    
+    public static bool IsForeignKeyViolation(System.Exception ex)
+    {
+        /*
+         * var code = GetErrorCode(ex);
+         * return code == ForeignKeyNoParentCode || code == ForeignKeyHasChildrenCode;
+         */
+        
+        var code = GetErrorCode(ex);
 
+        if (code == ForeignKeyNoParentCode || code == ForeignKeyHasChildrenCode)
+            return true;
+
+        System.Exception? current = ex;
+        while (current?.InnerException != null)
+            current = current.InnerException;
+
+        var message = current?.Message ?? ex.Message;
+
+        return message.Contains("FOREIGN KEY constraint fails", StringComparison.OrdinalIgnoreCase);
+    }
+    
     public static bool IsUniqueConstraintViolation(System.Exception ex) => GetErrorCode(ex) == DuplicateEntryCode;
     
-    public static bool IsForeignKeyViolation(System.Exception ex) 
-    {
-        var code = GetErrorCode(ex);
-        return code == ForeignKeyNoParentCode || code == ForeignKeyHasChildrenCode;
-    }
-
     public static bool IsDataTooLong(System.Exception ex) => GetErrorCode(ex) == DataTooLongCode;
 
     private static int GetErrorCode(System.Exception ex)
